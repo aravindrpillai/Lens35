434ff185dfe6c9b303408c4efc9a3cabe2faa0e6
Aravind Pillai <Aravind.Pillai@endava.com>
2023-03-14 21:08:54 +0000

    google maps

--- util/google_maps.py
+++ util/google_maps.py
@@ -1,6 +1,6 @@
 import googlemaps 
 # Requires API key 
-gmaps = googlemaps.Client(key='AIzaSyAxDro6aI_GmBD8bJo0M16EVXuYm8WtbhA')  
+gmaps = googlemaps.Client(key='secret_key')  
 # Requires cities name 
 distance = gmaps.distance_matrix('Rosemala, 691309','HSR Layout, 560102')['rows'][0]['elements'][0]
 
