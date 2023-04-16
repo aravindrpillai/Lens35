56da6abdcd4ccbfe2ced309b0be72c4bc2b51081
Aravind R Pillai <aravindrpillai1992@gmail.com>
2023-04-16 11:10:46 +0100

    Fetch Bookings API

--- apps/bookings/api_urls.py
+++ apps/bookings/api_urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from apps.bookings.apis.customers import handle_booking, add_services, remove_services, fetch_services, fetch_payment_report
+from apps.bookings.apis.customers import handle_booking, add_services, remove_services, fetch_services, fetch_payment_report, fetch_bookings
 from apps.bookings.apis.employees import list_bookings, fetch_booking_info, accept_booking, remove_booking
 
 urlpatterns = [
     
     #CUSTOMERS - _NOTE_: Start with customers only
     path(r'customers/booking/', handle_booking.index, name="URL to add or update booking"),
+    path('customers/bookings/fetch/', fetch_bookings.index, name="URL to fetch all the bookings of a customer"),
     path('customers/services/fetch/<uuid:booking_id>/', fetch_services.index, name="URL to fetch all the service of a booking"),
     path(r'customers/services/add/', add_services.index, name="URL to add new service(s)"),
     path(r'customers/services/remove/', remove_services.index, name="URL to remove service(s)"),
---
+++ apps/bookings/apis/customers/fetch_bookings.py
@@ -0,0 +1,66 @@
+from lens35.constanst import CUSTOMERS_BUCKET, CUSTOMERS_DP_FOLDER, EMPLOYEES_BUCKET, EMPLOYEES_DP_FOLDER
+from util.wasabi import get_presigned_url_to_access_object
+from apps.bookings.models.bookings import Bookings
+from apps.bookings.models.services import Services
+from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
+from util.http import build_response
+from util.logger import logger
+import traceback
+
+@api_view(['GET'])
+def index(request):
+    try:
+        customer_id = request.headers.get("Identifier")
+        response = []
+        for booking in Bookings.objects.fiilter(customer_id = customer_id):
+        
+            services = []
+            for service in Services.objects.filter(booking = booking).exclude(retired = True):
+                employee = None
+                if(service.employee != None):
+                    employee = {
+                        "employee_id" : service.employee.employee_id, 
+                        "full_name" : service.employee.full_name,
+                        "profile_name" : service.employee.profile_name,
+                        "display_picture" : __get_dp_url(service.employee.display_picture, "employee")
+                    }
+                services.append({
+                        "service_id" : service.service_id,
+                        "service" : service.service,
+                        "employee" : employee
+                    })
+
+            response.append({
+                    "booking_id" : booking.booking_id,
+                    "event" : booking.event,
+                    "event_date" : booking.event_date,
+                    "event_start_time" : booking.event_start_time,
+                    "event_duration" : booking.event_duration,
+                    "event_description" : booking.event_description,    
+                    "event_postal_code" : booking.event_postal_code,
+                    "event_city" : booking.event_city,
+                    "event_address" : booking.event_address,    
+                    "event_latitude" : booking.event_latitude,
+                    "event_longitude" : booking.event_longitude,
+                    "services": services
+                })
+        
+        return build_response(200, "Success", response)
+    except Exception as e_0:
+        logger.error('Failed to fetch the bookings for customer {}\n{}'.format(customer_id, traceback.format_exc()))
+        return build_response(400, str(e_0))
+    
+
+'''
+#Function to get the display pic full url
+'''
+def __get_dp_url(file_name, user):
+    if(file_name == None or file_name == ""):
+        return None
+    else:
+        if(user == "employee"):
+            return get_presigned_url_to_access_object(EMPLOYEES_BUCKET, EMPLOYEES_DP_FOLDER, file_name)
+        if(user == "customer"):
+            return get_presigned_url_to_access_object(CUSTOMERS_BUCKET, CUSTOMERS_DP_FOLDER, file_name)
+        
