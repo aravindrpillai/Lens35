b1e4575273209c357f71ce1aaa7a5cbde0ed0a0b
Aravind R Pillai <aravindrpillai1992@gmail.com>
2023-04-17 10:34:06 +0100

    folder fix

--- apps/bookings/apis/employees/presigned_url_for_file_upload.py
+++ apps/bookings/apis/employees/presigned_url_for_file_upload.py
@@ -1,5 +1,6 @@
 from util.wasabi import get_presigned_url_to_push_object, create_folder_inside_bucket
 from apps.bookings.models.bookings import Bookings
+from apps.bookings.models.services import Services
 from rest_framework.decorators import api_view
 from lens35.constanst import BOOKINGS_BUCKET
 from util.http import build_response
@@ -12,21 +13,24 @@ import uuid
 def index(request):
     try:
         data = request.data
-        
-        booking_id = data.get("booking_id", None)
+
+        service_id = data.get("service_id", None)
         mime_type = data.get("mime_type", None)
+        file_name = data.get("file_name", None)
+        is_photo = data.get("is_photo", None)
+
+        print("{} - {} - {}".format(file_name, mime_type, is_photo))
 
         #todo - verify if the b ooking id is associated to the right employee
-        #booking = Bookings.object.get(booking_id = booking_id)
+        #booking = Services.object.get(service_id = service_id)
 
-        if(mime_type == None or mime_type == "" or (not mime_type.startswith('image/'))):
-            raise Exception("Invalid File type. Please upload image files only")
-        file_extension = mimetypes.guess_extension(mime_type, strict=False)
+        # if(mime_type == None or mime_type == "" or (not mime_type.startswith('image/'))):
+        #     raise Exception("Invalid File type. Please upload image files only")
+        # file_extension = mimetypes.guess_extension(mime_type, strict=False)
 
-        create_folder_inside_bucket(BOOKINGS_BUCKET, booking_id)
+        create_folder_inside_bucket(BOOKINGS_BUCKET, service_id)
         
-        file_name = "{}{}".format(uuid.uuid4(), file_extension)
-        url_info = get_presigned_url_to_push_object(BOOKINGS_BUCKET, booking_id, file_name)
+        url_info = get_presigned_url_to_push_object(BOOKINGS_BUCKET, service_id, file_name)
         
         response = {
             "url" : url_info["url"],
