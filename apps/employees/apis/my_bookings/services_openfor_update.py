7909504b3f6e5246863619da5e1fd729f56743c1
aravindrpillai <aravindrpillai1992@gmail.com>
2023-05-31 09:31:16 +0000

    Services update

--- apps/employees/apis/my_bookings/services_openfor_update.py
+++ apps/employees/apis/my_bookings/services_openfor_update.py
@@ -1,3 +1,4 @@
+from apps.employees.models.employees import Employees
 from apps.bookings.models.services import Services
 from rest_framework.decorators import api_view
 from util.http import build_response
@@ -9,6 +10,7 @@ import traceback
 def index(request, booking_id):
     try:
         employee_id = request.headers.get("Identifier")
+        employee = Employees.objects.get(employee_id = employee_id)
         
         services = Services.objects.filter(
             Q(booking__booking_id=booking_id) & 
@@ -20,7 +22,8 @@ def index(request, booking_id):
             {
                 "service_id": service.service_id,
                 "service": service.service,
-                "is_still_open": service.employee is None
+                "is_still_open": service.employee is None,
+                "does_this_employee_offer_this_service" : __does_this_employee_offer_this_service(employee, service.service)
             }
             for service in services
         ]
@@ -29,3 +32,11 @@ def index(request, booking_id):
     except Exception as e_0:
         logger.error('Failed to fetch the bookings for employee {}\n{}'.format(employee_id, traceback.format_exc()))
         return build_response(400, str(e_0))
+        
+def __does_this_employee_offer_this_service(employee, service):
+    return (employee.is_photographer and service == "photography") or \
+   (employee.is_videographer and service == "videography") or \
+   (employee.is_drone_photographer and service == "drone_photography") or \
+   (employee.is_photo_editor and service == "photo_editing") or \
+   (employee.is_video_editor and service == "video_editing")
+
